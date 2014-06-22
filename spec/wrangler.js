b1f5977678d3308e397cf8a4e9d1d8f5ae75a1a7
Matt Styles <matt@veryfizzyjelly.com>
2014-06-22 20:58:04 +0100

    #add - unit tests for constructor functions

--- spec/wrangler.js
+++ spec/wrangler.js
@@ -3,7 +3,9 @@ var chai = require( 'chai' ),
     promised = require( 'chai-as-promised' ),
     rimraf = require( 'rimraf' ),
 
-    Wrangler = require( '../lib/wrangler' );
+    Wrangler = require( '../' ),
+    Model = require( '../lib/model' ),
+    Schema = require( '../lib/schema' );
 
 // Setup chai
 chai.should();
@@ -14,11 +16,13 @@ process.on( 'exit', function( status ) {
     rimraf.sync( dbName );
 });
 
-var wrangler,
+var wrangler, model, schema,
     dbName = './testdb'
 
 function before() {
     wrangler = null;
+    model = null;
+    schema = null;
     rimraf.sync( dbName );
 }
 
@@ -27,8 +31,12 @@ function perform( title, fn ) {
     test( title, fn );
 }
 
+function xperform( title, fn ) {
+    return;
+}
 
-suite( 'creating the wrangler instance', function() {
+
+suite( 'creating the wrangler instance - ', function() {
 
     perform( 'requiring wrangler should expose an object constructor', function() {
         Wrangler.should.be.a( 'function' );
@@ -47,7 +55,7 @@ suite( 'creating the wrangler instance', function() {
 });
 
 
-suite( 'adding models to wrangler', function() {
+suite( 'adding models to wrangler - ', function() {
 
     perform( 'models should be added and created if passed on creation', function( done ) {
 
@@ -67,6 +75,37 @@ suite( 'adding models to wrangler', function() {
                 wrangler.db.close( done );
             });
     });
+});
 
 
+suite( 'expose models and schema via wrangler - ', function() {
+
+    perform( 'model and schema constructor should be available from the wrangler object', function() {
+
+        Wrangler.Model.should.be.a( 'Function' );
+        Wrangler.Schema.should.be.a( 'Function' );
+    });
+
+    perform( 'schema constructor should create a schema', function() {
+
+        schema = new Wrangler.Schema({
+            foo: {
+                default: 'foo'
+            }
+        });
+
+        schema.should.be.an.instanceof( Schema );
+    });
+
+    perform( 'model constructor should create a model', function() {
+
+        model = new Wrangler.Model({
+            id: 'Users',
+            schema: new Wrangler.Schema({
+                foo: true
+            })
+        });
+
+        model.should.be.an.instanceof( Model );
+    })
 });
