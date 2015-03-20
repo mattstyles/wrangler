7b04f4db38cab6a59e600656f9043e7a6dfd6d13
mattstyles <matt@veryfizzyjelly.com>
2015-03-20 22:17:08 +0000

    #update clone props to model on instantiation

--- spec/model.js
+++ spec/model.js
@@ -30,14 +30,14 @@ test( 'Factories should be able to create Model instances', function( t ) {
     });
 
     t.ok( model instanceof ModelClass, 'Factory creates Models' );
-    t.ok( model.name === 'Chas', 'model is assigned properties' );
-    t.ok( users.cache.length === 1, 'model has been added to factory cache' );
+    t.equal( model.name, 'Chas', 'model is assigned properties' );
+    t.equal( users.cache.length, 1, 'model has been added to factory cache' );
 
-    var model2 = users.create({
+    users.create({
         name: 'Dave'
     });
 
-    t.ok( users.cache.length === 2, 'a second model has been added to factory cache' );
+    t.equal( users.cache.length, 2, 'a second model has been added to factory cache' );
 });
 
 
@@ -50,4 +50,36 @@ test( 'Models should be assigned a unique id upon creation', function( t ) {
     });
 
     t.ok( model.id.length > 8, 'Id assigned to model' );
+});
+
+
+test( 'Models should not pass by reference their props on instantiation', function( t ) {
+    t.plan( 5 );
+
+    var drivers = wrangler.createFactory( 'driver', {} );
+
+    var driver = {
+        name: 'Mansell'
+    };
+    var model = drivers.create( driver );
+
+    t.equal( model.name, 'Mansell', 'model name assigned from prop' );
+
+    driver.name = 'Ayrton';
+
+    t.notEqual( model.name, 'Ayrton', 'prop object should be independent of model instance' );
+    t.equal( model.name, 'Mansell', 'model should be unmodified after prop manipulation' );
+
+    t.equal( typeof model.save, 'function', 'prop clone to model should not nuke model prototype props' );
+
+    var driver2 = {
+        name: 'Niki',
+        save: 'foo'
+    };
+    t.throws( function() {
+        drivers.create( driver2 );
+    }, null, 'Model props name conflict with model prototype should throw' );
+
+
+
 });
