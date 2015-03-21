fdcfaee594ba518e017b901538c1f54abf6cf601
mattstyles <matt@veryfizzyjelly.com>
2015-03-21 22:01:57 +0000

    #add Factory:Find

--- spec/factory.js
+++ spec/factory.js
@@ -5,7 +5,9 @@ var test = require( 'tape' );
 
 var dbpath = path.join( os.tmpdir(), 'level-wrangler-' + Math.random() );
 
-var level = require( 'level' )( dbpath );
+var level = require( 'level' )( dbpath, {
+    encoding: 'json'
+});
 var wrangler = require( '../dist' )( level );
 
 var FactoryClass = require( '../dist/model/factory' );
@@ -67,4 +69,52 @@ test( 'Factory:Serialize should prepare a model for saving as json', function( t
     t.equal( typeof users.serialize( model2 ).speak, 'undefined', 'Serialize should strip functions from models' );
     t.equal( typeof s._private, 'undefined', 'Serialize should strip private props' );
 
+});
+
+
+test( 'Factory:Save should save a serialized model', function( t ) {
+    t.plan( 1 );
+
+    var users = wrangler.createFactory( 'user', {} );
+    var model = users.create({
+        name: 'Chas'
+    });
+
+    model.save()
+        .then( function() {
+            level.get( model.id, function( err, res ) {
+                if ( err ) {
+                    t.fail( err );
+                }
+
+                t.equal( res.name, 'Chas', 'saved object props match model props' );
+            });
+        })
+        .catch( t.fail );
+});
+
+
+test( 'Factory:Find should grab a saved model', function( t ) {
+    t.plan( 4 );
+
+    var users = wrangler.createFactory( 'user', {} );
+    var model = users.create({
+        name: 'Chas'
+    });
+
+    model.save()
+        .then( function() {
+            users.find( model.id )
+                .then( function( res ) {
+                    t.equal( res.name, 'Chas', 'found object props match model props' );
+
+                    t.doesNotThrow( function() {
+                        var model2 = users.create( res );
+                        t.equal( model2.name, 'Chas', 'Newly created model props match found instance' );
+                        t.notEqual( model.id, model2.id, 'Newly created model has unique id' );
+                    }, 'New model can be created from found instance' );
+                })
+                .catch( t.fail );
+        })
+        .catch( t.fail );
 });
